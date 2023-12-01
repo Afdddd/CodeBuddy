bae4eb032255461aef81393d32bd89be568b05e9
backdfb <wva22lfr@gmail.com>
2023-12-01 17:43:29 +0900

    자유게시판 Dao 작성 시작

---
+++ coddy/src/main/java/com/kh/coddy/board/free/model/dao/FboardDao.java
@@ -0,0 +1,12 @@
+package com.kh.coddy.board.code.model.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FboardDao {
+
+}
